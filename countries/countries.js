02091d060b98818d76f3b87c86d5b788143a5b2d
cepei-admin <connectingthedots@cepei.org>
2016-12-28 21:22:55 -0500

    Add files via upload

--- countries/countries.js
+++ countries/countries.js
@@ -102,8 +102,7 @@ angular.module('myApp.countries', ['ngRoute'])
 			  	var width = d3.select(this).node().getBoundingClientRect().width;
 		      	var odsIndex = d3.select(this).attr("id").split("-")[1];
 			  	d3.select(this)
-				
-				.style("top", -(height*($scope.relPercentODS(odsIndex))) + "px");
+
 				    .style("width", function(d){
 				      	return width*($scope.relPercentODS(odsIndex)) + "px";
 				    })
